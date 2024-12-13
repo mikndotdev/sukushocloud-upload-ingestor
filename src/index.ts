e11298b0e633468ac9f14df375258520684c4ffd
maamokun <maamo-kun@outlook.com>
2024-12-13 11:12:52 +0900

    add: missing file to git

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { Env } from "bun"
+import { app } from "./app"
+import { Context } from "elysia"
+
+export default {
+    async fetch(
+        request: Request,
+        env: Env,
+        ctx: Context,
+
+    ): Promise<Response> {
+
+        return await app.fetch(request)
+    },
+}
