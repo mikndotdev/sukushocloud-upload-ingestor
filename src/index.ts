b265678c388963fa4eb517bdef8675ebbf6cad6a
maamokun/MikanDev <maamo-kun@outlook.com>
2025-02-08 00:27:02 +0900

    feat: switch to Hono for Workers

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Env } from "bun"
-import { app } from "./app"
-import { Context } from "elysia"
-
-export default {
-    async fetch(request: Request, env: Env, ctx: Context): Promise<Response> {
-        return await app.fetch(request);
-    },
-};
